8dec81b2a2fabf1a97886744276dfecbe714d6c7
Erol SARICA <erol.sarica@boun.edu.tr>
2024-02-16 15:58:17 +0300

    curved bar added

---
+++ lib/curved_bottom_navigation_bar.dart
@@ -0,0 +1,46 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class CurvedNavigation extends StatefulWidget {
+  const CurvedNavigation({super.key});
+
+  @override
+  State<CurvedNavigation> createState() => _CurvedNavigationState();
+}
+
+class _CurvedNavigationState extends State<CurvedNavigation> {
+  int _page = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: Colors.blueAccent,
+        color: Colors.blueAccent,
+        animationDuration: const Duration(milliseconds: 200),
+        items: const <Widget>[
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.add, size: 30, color: Colors.white),
+          Icon(Icons.person, size: 30, color: Colors.white),
+        ],
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Selected page: $_page', style: const TextStyle(fontSize: 30),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'curved_bottom_navigation_bar.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: CurvedNavigation(),
+  )
+  );
+}
